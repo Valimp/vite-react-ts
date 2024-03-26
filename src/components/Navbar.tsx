4a1d6752f6260b676ab2bbe71613d8debbac4862
Valimp <valentinchauvet.contact@gmail.com>
2024-03-26 21:05:52 +0100

    fix: underline animation buttons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = (props: any) => {
     return (
         <header className="h-16 w-full shadow backdrop-blur-sm sticky top-0 flex justify-center items-center gap-5">
             <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Beetle.png" alt="Beetle" width="25" height="25" />
-            <nav className="flex gap-3">
+            <nav className="flex gap-5">
                 {
                     props.links.map((link: any, index: number) => {
                         return <TextButton key={index} to={link.to} content={link.content} />
--- src/components/TextButton.tsx
+++ src/components/TextButton.tsx
@@ -2,8 +2,9 @@ import { Link } from 'react-router-dom'
 
 const TextButton = (props: any) => {
   return (
-    <Link to={props.to} className='text-sm text-green-default hover:text-green-dark font-semibold'>
+    <Link to={props.to} className='group text-sm text-green-default hover:text-green-dark font-semibold transition duration-300'>
         {props.content}
+        <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-gradient-to-r from-green-default to-green-dark rounded"></span>
     </Link>
   )
 }
