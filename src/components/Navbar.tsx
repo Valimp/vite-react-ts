fd21ae1d7163ce515d5f16903ac368ccc4f61a2a
Valimp <valentinchauvet.contact@gmail.com>
2024-04-04 17:35:44 +0200

    fix: button menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,9 @@ import TextButton from "./TextButton"
 const Navbar = (props: any) => {
     return (
         <header className="h-16 w-full shadow backdrop-blur-lg bg-opacity-70 bg-white-default sticky top-0 flex justify-center items-center gap-5">
-            <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Beetle.png" alt="Beetle" width="25" height="25" />
+            <a href="/">
+                <img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Beetle.png" alt="Beetle" width="25" height="25" />
+            </a>
             <nav className="flex gap-5">
                 {
                     props.links.map((link: any, index: number) => {
